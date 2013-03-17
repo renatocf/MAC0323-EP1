0219dc3d393d33e746a5691c8052f26a1aaa03a1
Renato Cordeiro Ferreira <renatocordeiroferreira@gmail.com>
2013-03-16 21:17:21 -0300

    Modifying main.c to a final-version possibility of receive arguments

--- main.c
+++ main.c
@@ -3,26 +3,69 @@
 
 #include "getopt.h"
 
+typedef struct options {
+    int N;   /* Número de pontos */
+    int M;   /* Número de testes */
+    float d; /* Distância entre pontos */
+    int s;   /* Semente de números aleatórios */
+    int v;   /* Verbosidade nível 1 */
+    int V;   /* Verbosidade nível 2 */
+} Options;
+
+int receive_arguments(int argc, char **argv, Options *args);
+
 int main(int argc, char **argv)
 {
-    char opt;
-    int i = 0;
+    /** VARIÁVEIS *****************************************************/
+    Options args; /* Struct para os argumentos da linha de comando */
+    int result;
+    int i;
+    
+    /** ARGUMENTOS ****************************************************/
+        result = receive_arguments(argc, argv, &args);
+        if(result == EXIT_FAILURE) return EXIT_FAILURE;
     
     for(i = 0; i < argc; i++)
         printf("%s ", argv[i]);
     printf("\n");
     
-    while((opt = getopt(argc, argv, "v:ak::")) != NONE)
-    {
-        for(i = 0; i < argc; i++)
-            printf("%s ", argv[i]);
-        printf("\n");
-        
-        printf("option: %c\n", opt);
-        printf("optarg: %s\n", optarg);
-        printf("optopt: %c\n", optopt);
-        printf("optind: %d\n", optind);
-    }
-    
     return 0;
+}
+
+int receive_arguments(int argc, char **argv, Options *args)
+    /* Recebe os argumentos da linha de comando e os 
+     * armazena na struct correspondente */
+{
+    char opt; /* int i = 0; */
+    while((opt = getopt(argc, argv, "N:M:s:d:vV")) != NONE)
+    {
+        /* for(i = 0; i < argc; i++) */
+        /*     printf("%s ", argv[i]); */
+        /* printf("\n"); */
+        
+        /* printf("option: %c\n", opt); */
+        /* printf("optarg: %s\n", optarg); */
+        /* printf("optopt: %c\n", optopt); */
+        /* printf("optind: %d\n", optind); */
+        
+        switch(opt)
+        {
+            case 'N':
+                args->N = atoi(optarg); break;
+            case 'M':
+                args->M = atoi(optarg); break;
+            case 'd':
+                args->d = atof(optarg); break;
+            case 's':
+                args->s = atoi(optarg); break;
+            case 'v':
+                args->v = 1; break;
+            case 'V':
+                args->V = 1; break;
+            case '?':
+                printf("Argumento -%c desconhecido\n", optopt);
+                return EXIT_FAILURE;
+        }
+    } /* while */
+    return EXIT_SUCCESS;
 }
