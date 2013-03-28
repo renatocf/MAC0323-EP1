c2a88bcf9a26bc5d2b5532325240253c97460359
Renato Cordeiro Ferreira <renatocordeiroferreira@gmail.com>
2013-03-28 14:32:39 -0300

    Adding krustal library to git

---
+++ pqueue/krustal-item.h
@@ -0,0 +1,7 @@
+#ifndef H_KRUSTAL_ITEM_DEFINED
+#define H_KRUSTAL_ITEM_DEFINED
+
+#include "Point.h"
+typedef point KR_Item;
+
+#endif
---
+++ pqueue/krustal.h
@@ -0,0 +1,14 @@
+#ifndef H_KRUSTAL_DEFINED
+#define H_KRUSTAL_DEFINED
+
+/* Bibliotecas */
+#include "krustal-item.h"
+
+/* Definição de aresta de grafo */
+typedef struct edge Edge;
+typedef PLTree KRlist;
+
+/* Funções para uso do cliente */
+float edge_weight(Edge_l edges)
+
+#endif
